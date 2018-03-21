fba958369b6de235d4450a6454200cc304f2ba48
aarellan <aarellan@localhost.localdomain>
2018-03-21 10:15:26 +0100

    updated master and slave tests

--- producer_slave_test_execution.sh
+++ producer_slave_test_execution.sh
@@ -11,4 +11,4 @@ AMQ_SLAVE=slave
 AMQ_MASTER_HOME=$AMQ_INSTANCES/$AMQ_MASTER
 AMQ_SLAVE_HOME=$AMQ_INSTANCES/$AMQ_SLAVE
 
-$AMQ_MASTER_HOME/bin/artemis producer --message-count 10 --url "tcp://localhost:61616" --destination queue://haQueue
+$AMQ_MASTER_HOME/bin/artemis producer --message-count 10 --url "tcp://localhost:61716" --destination queue://haQueue
